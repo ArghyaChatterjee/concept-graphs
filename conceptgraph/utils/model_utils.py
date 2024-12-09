73e5950d265f1e34913dd0a11f49d82997ace365
ArghyaChatterjee <arghyame20buet@gmail.com>
2024-12-09 12:08:14 -0600

    changed model_utils.py

--- conceptgraph/utils/model_utils.py
+++ conceptgraph/utils/model_utils.py
@@ -129,10 +129,7 @@ def compute_clip_features(image, detections, clip_model, clip_preprocess, clip_t
 
     return image_crops, image_feats, text_feats
 
-<<<<<<< HEAD
-=======
 # @profile
->>>>>>> ali-dev
 def compute_clip_features_batched(image, detections, clip_model, clip_preprocess, clip_tokenizer, classes, device):
     
     image = Image.fromarray(image)
